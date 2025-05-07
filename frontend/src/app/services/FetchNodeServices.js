a984563dab2c10a2d525e48c84b7dad9d26d920d
manovaidya <manovaidya2@gmail.com>
2025-05-07 11:53:32 +0530

    update url

--- frontend/src/app/services/FetchNodeServices.js
+++ frontend/src/app/services/FetchNodeServices.js
@@ -2,8 +2,8 @@
 
 import axios from "axios";
 // const serverURL = 'https://api.manovaidya.com'
-//const serverURL = 'https://apiherbs.manovaidya.in'
-const serverURL = 'http://localhost:5000'
+const serverURL = 'https://apiherbs.manovaidya.in'
+// const serverURL = 'http://localhost:5000'
 
 const postData = async (url, body) => {
     try {
